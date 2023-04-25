2d9ccf1f31742158880ed97dabdfaf8484e300f5
Arun Kumar <118879115+Arun7398@users.noreply.github.com>
2023-04-25 09:16:33 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRouter from "./authentication/authentication.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+const client = new MongoClient(process.env.MONGO_URL);
+await client.connect();
+console.log("Mongodb connected...");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "https://password-reset-flow-react.netlify.app",
+    credentials: true
+  })
+);
+
+app.use("/auth", authRouter);
+
+app.get("/", (request, response) => {
+  response.send("welcome to my app...");
+});
+
+app.listen(PORT, () => console.log("app is running on port :", PORT, "⭐⭐⭐"));
+
+export { client };
